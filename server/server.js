6482b62e406a8fc31059ceacafa8f67aba0a9a54
Frank McDermott <fmcdermott4@gmail.com>
2021-07-21 15:51:53 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -19,12 +19,12 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "client\build")));
-}
+// if (process.env.NODE_ENV === "production") {}
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client\build\index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 db.once("open", () => {
