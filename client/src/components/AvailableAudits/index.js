17aedf5ccf8d4b00f0c033a07414836c87ad2af0
Frank McDermott <fmcdermott4@gmail.com>
2021-07-23 09:08:31 -0400

    audits page updated with map

--- client/src/components/AvailableAudits/index.js
+++ client/src/components/AvailableAudits/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-const AvailableAudits = () => {
-    return(
-    <h1>Available Audits</h1>
+const AvailableAudits = ({categories, title}) => {
+    if(!categories.length) {
+        return<h3>No available audits yet</h3>;
+    }
+
+    return(    
+        <div>
+            <h3 className="text-primary">{title}</h3>
+            <div className="flex-row justify-space-between my-4">
+        {categories &&
+          categories.map((category) => (
+            <div key={category._id} className="col-12 col-xl-6">
+              <div className="card mb-3">
+                <h4 className="card-header bg-dark text-light p-2 m-0">
+                  {category.category} <br />
+                </h4>
+
+                <Link
+                  className="btn btn-block btn-squared btn-light text-dark"
+                  to={`/profiles/${category._id}`}
+                >
+                  Conduct audit
+                </Link>
+              </div>
+            </div>
+          ))}
+      </div>
+        </div>
     )
 };
 
--- client/src/pages/Audits.js
+++ client/src/pages/Audits.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 import AvailableAudits from '../components/AvailableAudits';
+import { QUERY_CATEGORIES } from '../utils/queries';
 
 const Audits = () => {
+    const { loading, data } = useQuery(QUERY_CATEGORIES);
+    const categories = data?.categories || [];
+    
     return(
-        <div>
-    <h1>What audit do you want to conduct?</h1>
-    <AvailableAudits />
-    </div>
+        <main>
+            <div>            
+                {loading ? (<div>Loading...</div>) : (<AvailableAudits categories={categories} title="Here are the available audits:" />)}
+            </div>
+        </main>
     )
 };
 
