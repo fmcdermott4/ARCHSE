a61b1f6153be3dd995c3c505c903997966d7c6d2
Frank McDermott <fmcdermott4@gmail.com>
2021-07-26 13:33:02 -0400

    formState working now

--- client/src/components/IndividualAudit/index.js
+++ client/src/components/IndividualAudit/index.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import {useQuery} from '@apollo/client';
 import { QUERY_CATEGORY} from '../../utils/queries';
@@ -6,8 +6,9 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import Auth from '../../utils/auth';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
+// import DropdownButton from 'react-bootstrap/DropdownButton';
+// import Dropdown from 'react-bootstrap/Dropdown';
+
 
 
 
@@ -21,38 +22,44 @@ const IndividualAudit = () => {
         {
             variables: { id: categoryId },
         }
-    );
-    
+    );    
     const answersState = (loading, data) =>{
         if(!loading){
         let el = {};
         el.profile = myProfileId;
         el.category = categoryId;
         el.timeSubmitted = new Date();
-        el.answeredQuestions = []
         for ( let i = 0; i < data.category.questions.length; i++) {
             let index = data.category.questions[i]._id
-            el.answeredQuestions[index] = '';
+            el[index] = '';
         }
-        el.profile = myProfileId;
-        el.category = categoryId;
         return(el)
         }
     };
 
     const [formState, setFormState] = useState(answersState(loading, data));
 
-    let questionsMap = (data) => {
-        let questions = data.category.questions.map((question) => {
-            const answerBlock = question.answers.map((answer) =>{      
-                return(<Dropdown.Item eventKey={answer}>{answer}</Dropdown.Item>)
-                // return(<option key={answer} value={answer}>{answer}</option>)
-            })
-            return(<Form.Group as={Row} id={question._id} key={question._id} className="mb-3">{question.question}<DropdownButton alignRight title="update me">{answerBlock}</DropdownButton><Dropdown.Divider /></Form.Group>);
-            // return(<Form.Group as={Row} id={question._id} key={question._id} className="mb-3">{question.question}<Form.Control as="select"><option>Please select an answer</option>{answerBlock}</Form.Control></Form.Group>);
+
+    const handleSelect = (event) => {
+        const {name, value} = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
         })
-        return(<div><Form>{questions}</Form><div align="center"><Button variant="primary">Submit</Button></div></div>);
     }
+    let questionsMap = (loading, data) => {
+        if(!loading){
+            let questions = data.category.questions.map((question) => {
+                const answerBlock = question.answers.map((answer) =>{      
+                    // return(<option value={answer} >{answer}</option>)
+                    return(<option key={answer} value={answer}>{answer}</option>)
+                })
+                // return(<Form.Group as={Row} key={question._id} className="mb-3">{question.question}<Select id={question._id} onChange={this.handleSelect} title="name">{answerBlock}</Select><Dropdown.Divider /></Form.Group>);
+                return(<Form.Group as={Row} id={question._id} key={question._id} className="mb-3">{question.question}<Form.Control as="select" name={question._id} onChange={handleSelect}><option>Please select an answer</option>{answerBlock}</Form.Control></Form.Group>);
+            })
+            return(<div><Form>{questions}</Form><div align="center"><Button variant="primary">Submit</Button></div></div>);
+        }
+    };
 
     // let questionsMap = (data) => {
     //     return(<div></div>);
@@ -65,7 +72,7 @@ const IndividualAudit = () => {
                 (<h2>{data.category.category}</h2>            
                 )
             }
-            {(loading) ? (<div></div>) : (questionsMap(data))}
+            {(loading) ? (<div></div>) : (questionsMap(loading, data))}
         </div>
     )
 
