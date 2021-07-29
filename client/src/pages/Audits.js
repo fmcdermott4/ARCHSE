e19195486a8be1c79fa10c73acf83ef04844acdc
Frank McDermott <fmcdermott4@gmail.com>
2021-07-29 10:50:12 -0400

    need to stash this

--- client/src/pages/Audits.js
+++ client/src/pages/Audits.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import AvailableAudits from '../components/AvailableAudits';
-import { QUERY_CATEGORIES } from '../utils/queries';
+import { QUERY_CATEGORIES, QUERY_AUDIT_TYPES } from '../utils/queries';
 import Auth from '../utils/auth';
 
 const Audits = () => {
     const { loading, data } = useQuery(QUERY_CATEGORIES);
     const categories = data?.categories || [];
+    const auditTypes = useQuery(QUERY_AUDIT_TYPES).data;
+    const auditTypesButton = (auditTypes) =>{
+        
+        if(typeof(auditTypes) === 'undefined'){
+            return("Loading...")
+        } else{ 
+            const button = () =>{
+                auditTypes.auditTypes.map((audit) => {
+                    console.log(audit.auditType)
+                })
+            }
+            
+            console.log(button())
+        }
+    };
     
     return(
         <main>
             <div>
-                {(loading) ? (<div>Loading...</div>) : (<div></div>)}            
+                {(loading) ? (<div>Loading...</div>) : (<div></div>)}  
+                {auditTypesButton(auditTypes)}          
                 {Auth.loggedIn() ? (<AvailableAudits categories={categories} title="Available audits:" />) : (<p>Please <a href="/login">log in</a> to view this content.</p>)}
             </div>
         </main>
